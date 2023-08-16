9c9062c17f1e2c44153ace1a306ddee6d480d09e
Lucas Petit-LeTirilly <lucas.letirilly.petit@gmail.com>
2023-08-16 22:14:33 +0200

    Working on drawer component

---
+++ app/components/SwipeableTemporaryDrawer.tsx
@@ -0,0 +1,81 @@
+import * as React from "react";
+import Box from "@mui/material/Box";
+import SwipeableDrawer from "@mui/material/SwipeableDrawer";
+import Button from "@mui/material/Button";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemText from "@mui/material/ListItemText";
+import Image from "next/image";
+
+type Anchor = "top" | "left" | "bottom" | "right";
+
+export default function SwipeableTemporaryDrawer() {
+  const [state, setState] = React.useState({
+    top: false,
+    left: false,
+    bottom: false,
+    right: false,
+  });
+
+  const toggleDrawer =
+    (anchor: Anchor, open: boolean) =>
+    (event: React.KeyboardEvent | React.MouseEvent) => {
+      if (
+        event &&
+        event.type === "keydown" &&
+        ((event as React.KeyboardEvent).key === "Tab" ||
+          (event as React.KeyboardEvent).key === "Shift")
+      ) {
+        return;
+      }
+
+      setState({ ...state, [anchor]: open });
+    };
+
+  const list = (anchor: Anchor) => (
+    <Box
+      sx={{ width: anchor === "top" || anchor === "bottom" ? "auto" : 250 }}
+      role="presentation"
+      onClick={toggleDrawer(anchor, false)}
+      onKeyDown={toggleDrawer(anchor, false)}
+    >
+      <List>
+        {["Projets Récents", "Compétences", "Contact", "In English"].map(
+          (text) => (
+            <ListItem key={text} disablePadding>
+              <ListItemButton>
+                <ListItemText primary={text} />
+              </ListItemButton>
+            </ListItem>
+          ),
+        )}
+      </List>
+    </Box>
+  );
+
+  return (
+    <div className="sm:hidden">
+      {(["right"] as const).map((anchor) => (
+        <React.Fragment key={anchor}>
+          <Button onClick={toggleDrawer(anchor, true)}>
+            <Image
+              src="bars-solid.svg"
+              alt="Ouvrir le menu de navigation"
+              width={40}
+              height={45.71}
+            />
+          </Button>
+          <SwipeableDrawer
+            anchor={anchor}
+            open={state[anchor]}
+            onClose={toggleDrawer(anchor, false)}
+            onOpen={toggleDrawer(anchor, true)}
+          >
+            {list(anchor)}
+          </SwipeableDrawer>
+        </React.Fragment>
+      ))}
+    </div>
+  );
+}
--- app/components/navBar.tsx
+++ app/components/navBar.tsx
@@ -1,48 +1,57 @@
 "use client";
 import Image from "next/image";
 import Link from "next/link";
+import SwipeableTemporaryDrawer from "./SwipeableTemporaryDrawer";
+import { createTheme, ThemeProvider } from "@mui/material";
+
+const theme = createTheme({
+  typography: {
+    fontFamily: "Koho, sans-serif",
+    body1: {
+      fontWeight: 500,
+      fontSize: 24,
+      color: "white",
+    },
+  },
+  palette: {
+    background: {
+      paper: "#76453F",
+    },
+  },
+});
 
 export default function NavBar() {
-  function sayHello() {
-    alert("You clicked me!");
-  }
   return (
-    <header className="bg-brown-header flex flex-row justify-between items-center pl-5 pr-5 pt-2 pb-2">
-      <Link href={process.env.NEXT_PUBLIC_WEBSITE_URL}>
-        <Image
-          src="logo-home.svg"
-          alt="Retour à l'accueil"
-          width={40}
-          height={40}
-        />
-      </Link>
-      <Link href={process.env.NEXT_PUBLIC_GITHUB_URL}>
-        <Image
-          src="logo-github.svg"
-          alt="Lien vers la page github de Lucas Petit - Le Tirilly"
-          width={40}
-          height={40}
-        />
-      </Link>
-      <Link href={process.env.NEXT_PUBLIC_LINKEDIN_URL}>
-        <Image
-          src="logo-linkedin.svg"
-          alt="Lien vers la page linked in de Lucas Petit - Le Tirilly"
-          width={40}
-          height={40}
-        />
-      </Link>
-      <nav>
-        <button onClick={sayHello}>
+    <ThemeProvider theme={theme}>
+      <header className="bg-brown-header flex flex-row justify-between items-center pl-5 pr-5 pt-2 pb-2">
+        <Link href={process.env.NEXT_PUBLIC_WEBSITE_URL}>
           <Image
-            src="bars-solid.svg"
-            alt="Ouvrir le menu de navigation"
+            src="logo-home.svg"
+            alt="Retour à l'accueil"
             width={40}
-            height={45.71}
-            className="sm:hidden"
+            height={40}
           />
-        </button>
-      </nav>
-    </header>
+        </Link>
+        <Link href={process.env.NEXT_PUBLIC_GITHUB_URL}>
+          <Image
+            src="logo-github.svg"
+            alt="Lien vers la page github de Lucas Petit - Le Tirilly"
+            width={40}
+            height={40}
+          />
+        </Link>
+        <Link href={process.env.NEXT_PUBLIC_LINKEDIN_URL}>
+          <Image
+            src="logo-linkedin.svg"
+            alt="Lien vers la page linked in de Lucas Petit - Le Tirilly"
+            width={40}
+            height={40}
+          />
+        </Link>
+        <nav>
+          <SwipeableTemporaryDrawer />
+        </nav>
+      </header>
+    </ThemeProvider>
   );
 }
