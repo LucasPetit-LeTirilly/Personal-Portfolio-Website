3dbc27d57ca58e60f7293e2c0af31c1798423a79
Lucas Petit-LeTirilly <lucas.letirilly.petit@gmail.com>
2023-09-25 21:02:52 +0200

    Changed smtp transporter

--- app/api/form/route.ts
+++ app/api/form/route.ts
@@ -4,24 +4,21 @@ import { NextResponse } from "next/server";
 const nodemailer = require("nodemailer");
 
 let transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.sendgrid.net",
+  port: 465,
   auth: {
-    type: "OAuth2",
     user: process.env.MAIL_USERNAME,
     pass: process.env.MAIL_PASSWORD,
-    clientId: process.env.OAUTH_CLIENTID,
-    clientSecret: process.env.OAUTH_CLIENT_SECRET,
-    refreshToken: process.env.OAUTH_REFRESH_TOKEN,
   },
 });
 
 export async function POST(request: Request) {
   const res = await request.json();
   let mailOptions = {
-    from: res.email,
+    from: "lucaspetitletirilly.formsend@gmail.com",
     to: "lucaspetitletirilly.formsend@gmail.com",
     subject: res.subject,
-    text: `Nom: ${res.surname}, Prenom: ${res.name}, Message:${res.message}`,
+    text: `Nom: ${res.surname}, Prenom: ${res.name}, Message:${res.message} Email: ${res.email}`,
   };
   try {
     await transporter.sendMail(mailOptions);
